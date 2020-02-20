964536b8ad1c66cf27844f1016baf8e719742b3e
max-jacobi <mjacobi@theorie.ikp.tu-darmstadt.de>
2020-02-20 09:26:13 +0100

    clean up

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author="Max Jacobi",
     author_email="mjacobi@theorie.ikp.physik.tu-darmstadt.de",
     packages=['flowplot', 'flowplot/plots', 'flowplot/flow'],
+    include_package_data=True,
     long_description=open('README.md').read(),
     description="Some scripts to plot flows from WinNet"
 )
